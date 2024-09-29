6d19815f3d891a27b0f5a95728758fc1b5ae8db4
Nathaniel Clizbe <nathanielclizbe@crc-dot1x-nat-10-239-52-126.bu.edu>
2024-09-29 15:32:40 -0400

    updated allowed hosts

--- cs412/settings.py
+++ cs412/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-eu1^&84hpbu&qk+af7z!&_ps1h^dwdddf=apzjt33r-+hn$5+p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://twain-quotes-6f7db3879a82.herokuapp.com/", '127.0.0.1']
+ALLOWED_HOSTS = ["https://twain-quotes-6f7db3879a82.herokuapp.com/", '127.0.0.1', "theflyingstonecanteen-45ffd096eafa.herokuapp.com"]
 
 
 # Application definition
