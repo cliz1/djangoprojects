2d09b22f9d7e9bdeb01a3df2d96f2a2bada56bdf
Nathaniel Clizbe <nathanielclizbe@crc-dot1x-nat-10-239-26-231.bu.edu>
2024-10-10 18:29:54 -0400

    added profile pages and status messages

--- mini_fb/views.py
+++ mini_fb/views.py
@@ -5,10 +5,17 @@ from django.shortcuts import render
 #from django.shortcuts import render
 from django.shortcuts import render
 from .models import Profile
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 class ShowAllProfilesView(ListView):
     '''Create a subclass of ListView to display all mini_fb Profiles.'''
     model = Profile # retrieve objects of type Article from the database
     template_name = 'mini_fb/show_all_profiles.html'
     context_object_name = 'profiles' # how to find the data in the template file
+
+
+class ProfileView(DetailView):
+    '''Display one Profile selected by PK'''
+    model = Profile # the model to display
+    template_name = "mini_fb/show_profile.html"
+    context_object_name = "profile"
