1ef853cde18c6005d6456458b032c41fc5126164
Nathaniel Clizbe <nathanielclizbe@crc-dot1x-nat-10-239-115-138.bu.edu>
2024-11-10 15:27:14 -0500

    added task 3 for assignment 10

--- voter_analytics/urls.py
+++ voter_analytics/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', VoterListView.as_view(), name='voters'),
     path('voter/<int:pk>/', views.VoterDetailView.as_view(), name='voter'),
+    path('graphs/', views.VoterGraphsView.as_view(), name='graphs')
 ]
